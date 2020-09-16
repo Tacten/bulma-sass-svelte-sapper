a47424954d7057ede3937243537111c55e71994e
atul@tacten.co <atul@tacten.co>
2020-09-16 08:43:52 +0530

    bulma sass

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -20,7 +20,27 @@
       
 	  <a class="navbar-item" aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a>
   <a class="navbar-item" rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a>
-     
+     <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          More
+        </a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            About
+          </a>
+          <a class="navbar-item">
+            Jobs
+          </a>
+          <a class="navbar-item">
+            Contact
+          </a>
+          <hr class="navbar-divider">
+          <a class="navbar-item">
+            Report an issue
+          </a>
+        </div>
+      </div>
     </div>
 
     <div class="navbar-end">
